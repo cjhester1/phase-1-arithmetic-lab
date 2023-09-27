1f68ef2b5ed8a49eac20f3cd7a7424dba89b94a2
cjhester1 <cjhester23@gmail.com>
2023-09-26 22:36:32 -0700

    arithmetic lab solution

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random()) +1
+
+console.log(random);
+
+let num3 = 28
+let num4 = 4;
+const mod = 28%6;
+
+let max = Math.max(1,2,3,4,5,20)
